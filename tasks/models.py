ff72b9bb06dfb31ef90f18dd7c46b83aa470201c
Nadezhda Khovrich <hope28281@gmail.com>
2022-09-17 12:38:59 +0600

    Создание модели Task
    миграция в базу

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    description = models.TextField(verbose_name='Задание', max_length=200, null=False, blank=False)
+    status = models.CharField(
+        verbose_name='Статус', 
+        max_length=15, 
+        null=False, 
+        blank=False, 
+        default='New')
+    deadline = models.DateField(verbose_name='Дата выполнения', default=None)
+
+
+    def __str__(self):
+        return f"{self.description}"
+        
