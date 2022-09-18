8a12a302d7f233e518d6ec701bc8fba63848e588
Nadezhda Khovrich <hope28281@gmail.com>
2022-09-18 10:57:41 +0600

    Создание главной страницы index.html
    отображение текущих задач на главной странице

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from tasks.models import Task
 
 def index_view(request):
-    return render(request=request, template_name='index.html')
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request=request, template_name='index.html', context=context)
